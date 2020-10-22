bde70453fe5e8b35ca620dc7a13e1a93e9d6f472
jzhowie <jzhou20@stuy.edu>
2020-10-22 14:35:24 -0400

    Added sumRows method

--- ArrayOps.java
+++ ArrayOps.java
@@ -1,4 +1,12 @@
 public class ArrayOps {
+public static int[] sumRows(int[][] matrix) {
+	int newarr[] = new int[matrix.length];
+	for (int row = 0; row < matrix.length; row++) {
+		newarr[row] = largest(matrix[row]);
+	}
+	return newarr;
+}
+
 public static int sum(int[] arr) {
 	int sum = 0;
 	for (int i = 0; i < arr.length; i++) {
