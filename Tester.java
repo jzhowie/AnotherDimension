dc9ee5dce597cf6325a0e15b5da18579b5b5266e
jzhowie <jzhou20@stuy.edu>
2020-10-22 14:35:08 -0400

    Imported java arrays and added test cases for sumRows method

--- Tester.java
+++ Tester.java
@@ -1,7 +1,13 @@
+import java.util.Arrays;
+
 public class Tester {
 public static void main(String[] args) {
 	int[] A = {1, 3, 41, 5};
+	int[][] B = { {1, 3, 51, 134}, {5, 1, 153, 13541}, {314, 641, 134, 42}};
+	int[][] C = { {-1, -3, -51, -134}, {5, 1, 153, -13541}, {-314, -641, 134, -42}};
 	System.out.println(ArrayOps.sum(A));
 	System.out.println(ArrayOps.largest(A));
+	System.out.println(Arrays.toString(ArrayOps.sumRows(B)));
+	System.out.println(Arrays.toString(ArrayOps.sumRows(C)));
 }
 }
