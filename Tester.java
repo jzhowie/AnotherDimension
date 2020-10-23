d54690825c13f3a9535cda6ac305670a0517f9e5
jzhowie <jzhou20@stuy.edu>
2020-10-23 13:24:36 -0400

    Added tester cases for isRowsMagic method

--- Tester.java
+++ Tester.java
@@ -3,8 +3,10 @@ import java.util.Arrays;
 public class Tester {
 public static void main(String[] args) {
 	int[] A = {1, 3, 41, 5};
-	int[][] B = { {0, 5, 10, 15}, {0, -5, -10, -15}, {999, 1, 0, 0}};
-	int[][] C = { {10000, 0, 1, 0}, {-1, -1, -1, -1}, {0, 1, 0, 1}};
+	int[][] B = { {0, 5, 10, 15}, {0, -5, -10, -15}, {999, 1, 0, 0} };
+	int[][] C = { {10000, 0, 1, 0}, {-1, -1, -1, -1}, {0, 1, 0, 1} };
+	int[][] D = { {10}, {10}, {10} };
+	int[][] E = { {10, 30, 20, 40}, {10, 40, 20, 30}, {10, 50, 20, 20} };
 	int[][] TestCaseA = { {1, 0, 12, -1}, {7, -2, 2, 1}, {-5, -2, 2, -9} };
 	int[] TestCaseB = {1, 3, 5};
 
@@ -34,5 +36,9 @@ public static void main(String[] args) {
 	System.out.println(Arrays.toString(ArrayOps.sumCols(B)));
 	System.out.println(Arrays.toString(ArrayOps.sumCols(C)));
 	System.out.println(Arrays.toString(ArrayOps.sumCols(TestCaseA)));
+
+	System.out.println("Sum of rows the same?");
+	System.out.println(ArrayOps.isRowMagic(D));
+	System.out.println(ArrayOps.isRowMagic(E));
 }
 }
