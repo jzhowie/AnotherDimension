c720d89c88bcb8e1b69b19b7a050565180945709
jzhowie <jzhou20@stuy.edu>
2020-10-23 13:12:23 -0400

    Added test cases for previous methods and sumCols

--- Tester.java
+++ Tester.java
@@ -5,8 +5,12 @@ public static void main(String[] args) {
 	int[] A = {1, 3, 41, 5};
 	int[][] B = { {0, 5, 10, 15}, {0, -5, -10, -15}, {999, 1, 0, 0}};
 	int[][] C = { {10000, 0, 1, 0}, {-1, -1, -1, -1}, {0, 1, 0, 1}};
+	int[][] TestCaseA = { {1, 0, 12, -1}, {7, -2, 2, 1}, {-5, -2, 2, -9} };
+	int[] TestCaseB = {1, 3, 5};
+
 	System.out.println("Sum of 1D array:");
 	System.out.println(ArrayOps.sum(A));
+	System.out.println(ArrayOps.sum(TestCaseB));
 
 	System.out.println("Largest of 1D array:");
 	System.out.println(ArrayOps.largest(A));
@@ -14,13 +18,21 @@ public static void main(String[] args) {
 	System.out.println("Sum of each row in 2D array:");
 	System.out.println(Arrays.toString(ArrayOps.sumRows(B)));
 	System.out.println(Arrays.toString(ArrayOps.sumRows(C)));
+	System.out.println(Arrays.toString(ArrayOps.sumRows(TestCaseA)));
 
 	System.out.println("Largest of each row in 2D array:");
 	System.out.println(Arrays.toString(ArrayOps.largestInRows(B)));
 	System.out.println(Arrays.toString(ArrayOps.largestInRows(C)));
+	System.out.println(Arrays.toString(ArrayOps.largestInRows(TestCaseA)));
 
 	System.out.println("Sum of everything in 2D array:");
 	System.out.println(ArrayOps.sum(B));
 	System.out.println(ArrayOps.sum(C));
+	System.out.println(ArrayOps.sum(TestCaseA));
+
+	System.out.println("Sum of cols:");
+	System.out.println(Arrays.toString(ArrayOps.sumCols(B)));
+	System.out.println(Arrays.toString(ArrayOps.sumCols(C)));
+	System.out.println(Arrays.toString(ArrayOps.sumCols(TestCaseA)));
 }
 }
